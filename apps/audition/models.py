957905283b1c230c0972bbb0ebcf8c106ae86060
Pond <lastsecond1989@gmail.com>
2025-04-15 01:38:07 +0700

    create Audition model

--- apps/audition/models.py
+++ apps/audition/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+class Audition(models.Model): 
+
+    """
+    annc stands for annoucement
+    conc stands for concert
+    """
+
+    name = models.CharField(max_length=100)
+    deadline = models.DateTimeField("dl", null=True, blank=True)
+    annc_date = models.DateTimeField("annc", null=True, blank=True)
+    conc_date = models.DateTimeField("conc", null=True, blank=True)
+    poster = models.ImageField(upload_to="audition/posters/")
+
+    def __str__(self):
+        return f"{self.id} {self.name}"
