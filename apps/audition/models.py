dd620fac38621dcf97fe6e4def23a429773f9a85
Pond <lastsecond1989@gmail.com>
2025-04-20 16:26:20 +0700

    add datetime_default func. / modify the display names of annc_dt, conc_dt fields in the Audition templates

--- apps/audition/models.py
+++ apps/audition/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
+from config.utils import datetime_default
+
 class Audition(models.Model): 
 
     """
     annc stands for annoucement
     conc stands for concert
+    dt stands for date
     """
 
     name = models.CharField(max_length=100)
-    deadline = models.DateTimeField("dl", null=True, blank=True)
-    annc_date = models.DateTimeField("annc", null=True, blank=True)
-    conc_date = models.DateTimeField("conc", null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True, default=datetime_default)
+    annc_dt = models.DateTimeField("announcement date", null=True, blank=True, default=datetime_default)
+    conc_dt = models.DateTimeField("concert", null=True, blank=True, default=datetime_default)
     poster = models.ImageField(upload_to="audition/posters/")
 
     def __str__(self):
---
+++ config/utils.py
@@ -0,0 +1,4 @@
+from datetime import datetime, time
+
+def datetime_default():
+    return datetime.combine(datetime.now(), time(15, 0))
