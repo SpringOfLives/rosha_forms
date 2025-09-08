3878d3900bb3b606d306c087095f469357a1f3ff
Pond <lastsecond1989@gmail.com>
2025-09-08 13:43:12 +0700

    config media settings

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
@@ -11,3 +13,6 @@ urlpatterns = [
     # For testing
     path('', TemplateView.as_view(template_name="base.html"))
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
