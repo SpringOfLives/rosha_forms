00b348c744690c864dbf255e94710936ecd3dd08
juon <lastsecond1989@gmail.com>
2025-10-29 01:17:04 +0700

    fix apps.py in auditioner app

--- apps/auditioner/apps.py
+++ apps/auditioner/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuditionerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auditioner'
+    name = 'apps.auditioner'
