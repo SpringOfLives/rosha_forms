65e1ac27db37301512923b411454fa405a281545
juon <lastsecond1989@gmail.com>
2025-11-03 06:13:40 +0700

    create auditioner list page

--- apps/auditioner/urls.py
+++ apps/auditioner/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name = "auditioner"
+
+urlpatterns = [
+   path("", views.AuditionerListView.as_view(), name="auditioner-list"),
+]
