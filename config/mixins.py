561b104ab8e771fe382c68c74f905c2cd66b367a
juon <lastsecond1989@gmail.com>
2025-11-05 13:39:29 +0700

    create mixins class for checking user access to objects in views

--- config/mixins.py
+++ config/mixins.py
@@ -1,4 +1,7 @@
 from django.forms.widgets import SplitDateTimeWidget
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.views import redirect_to_login
+from django.http import Http404
 
 
 class FormMixins:
@@ -35,3 +38,89 @@ class FormMixins:
                     widget.widgets[1].attrs.setdefault("placeholder", placeholder)
             else:
                 widget.attrs.setdefault("placeholder", placeholder)
+
+class SuperuserRequiredMixin(UserPassesTestMixin):
+
+    raise_exception = True
+
+    def test_func(self) -> bool:
+        """Test if the user is a superuser.
+
+        Returns:
+            bool: True if the user is a superuser, otherwise False.
+        """
+        return self.request.user.is_superuser if self.request.user.is_authenticated else False
+
+    def handle_no_permission(self):
+        """Handle the case when the user does not have the required permission.
+
+        Returns:
+            HttpResponse: Redirect to the login page or raise a 404 exception
+            if the user is authenticated.
+        """
+        if self.raise_exception and self.request.user.is_authenticated:
+            raise Http404
+        return redirect_to_login(
+            self.request.get_full_path(),
+            self.get_login_url(),
+            self.get_redirect_field_name(),
+        )
+    
+class StaffRequiredMixin(UserPassesTestMixin):
+
+    raise_exception = True
+
+    def test_func(self) -> bool:
+        """Test if the user is a staff.
+
+        Returns:
+            bool: True if the user is a staff, otherwise False.
+        """
+        return self.request.user.is_staff if self.request.user.is_authenticated else False
+
+    def handle_no_permission(self):
+        """Handle the case when the user does not have the required permission.
+
+        Returns:
+            HttpResponse: Redirect to the login page or raise a 404 exception if the user is authenticated.
+        """
+        if self.raise_exception and self.request.user.is_authenticated:
+            raise Http404
+        return redirect_to_login(
+            self.request.get_full_path(),
+            self.get_login_url(),
+            self.get_redirect_field_name(),
+        )
+    
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import Http404
+from django.contrib.auth.views import redirect_to_login
+
+class UserObjectPermissionMixin(UserPassesTestMixin):
+    """
+    Mixin to control access based on user level:
+    - Staff or higher can access any object.
+    - Active users can only access objects they own (object.user).
+    """
+    raise_exception = True
+
+    def test_func(self) -> bool:
+        user = self.request.user
+        obj = self.get_object()
+
+        if not user.is_authenticated:
+            return False
+
+        if user.is_staff:
+            return True
+
+        return user.is_active and hasattr(obj, 'user') and user.id == obj.user.id
+
+    def handle_no_permission(self):
+        if self.raise_exception and self.request.user.is_authenticated:
+            raise Http404
+        return redirect_to_login(
+            self.request.get_full_path(),
+            self.get_login_url(),
+            self.get_redirect_field_name(),
+        )
